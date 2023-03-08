1ee96ba8b2b65ee8b7d0634d9130b13b3c0ffd6d
sen <sen121108@icloud.com>
2023-03-08 17:06:59 +0900

    makePostFunction

--- src/component/pages/Create.tsx
+++ src/component/pages/Create.tsx
@@ -1,8 +1,89 @@
+import axios from "axios";
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+interface FormData {
+    name: string;
+    money: number;
+    appendix: string;
+}
+//インデント整形　macOS ： Shift + Option + F 
+
 export const Create: React.FC = () => {
+
+    const [formData, setFormData] = useState<FormData>({
+        name: '',
+        money: 0,
+        appendix: 'N/A',
+    })
+    const [isOk,setIsOk] = useState(false);
+
+    const handleInputChange = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const navigate = useNavigate()
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if(formData.name === '' || isNaN(formData.money) ) {
+            setIsOk(true);
+        }else {
+            const url = 'http://localhost:8888/';
+                try {
+                    const response = await axios.post(url, formData);
+                    console.log(response.data);
+                    navigate('/')
+                } catch (error) {
+                    console.error(error);
+                }
+            };  
+        }
+
     return (
-        <div>
-            <p>create</p>
+
+        <div className=" text-center mt-10 ">
+            <h1>Post Form</h1>
+            <div>
+            {isOk && <p className="text-red-500">データの送信に失敗しました。名前を記入し、金額を半角数字で記入してください。</p>}
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">名前 : </label>
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                        className=" border mt-5"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="money">金額 : </label>
+                    <input
+                        name="money"
+                        value={formData.money}
+                        className=" border mt-10"
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="append">補足事項 : </label>
+                    <input
+                        name="appendix"
+                        value={formData.appendix}
+                        onChange={handleInputChange}
+                        className=" border mt-10"
+                    />
+                </div>
+                <button type="submit" className="mt-10 mb-10 border hover:text-red-300 hover:opacity-75 transition ">Submit</button>
+                <Link to="/">
+                <p>ホームに戻る</p>
+                </Link>
+            </form>
+            </div>
         </div>
     )
-    
+
 }
--- src/component/pages/index.tsx
+++ src/component/pages/index.tsx
@@ -1,17 +1,39 @@
 import { GetApiItem } from "../../api/getItem"
+import { Link } from "react-router-dom"
 
 export const Index: React.FC = () => {
     const items:any = GetApiItem()
     console.log(items[0])
     return (
         <div>
-              <ul>
-                {Object.keys(items).map(key => (
-                  <li key={key}>
+          <div>
+            <Link to='/create' className="text-center m-5 hover:scale-150 ">
+              <p className="hover:scale-150 hover:opacity-75 transition"  >post画面に移行する</p>
+            </Link>
+          </div>
+          <table className="m-auto mt-10">
+            <tbody>
+            <tr>
+              <th>名前</th>
+              <th>金額</th>
+              <th>補足</th>
+            </tr>
+            {Object.keys(items).map(key => (
+                  <tr key={key}>
+                  <td>
                     {items[key].name}
-                  </li>
+                  </td>
+                  <td>
+                    {items[key].money}
+                  </td>
+                  <td>
+                    {items[key].appendix}
+                  </td>
+                </tr>
                 ))}
-              </ul>
+              </tbody>
+          </table>
+          
         </div>
     )
     
